6389b042d903f4207ff0c8f0f911cf2172d7e906
v-teja <tejaviswa95@gmail.com>
2023-05-19 16:43:44 +0530

    Attemp-v2

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,9 @@ const App = () => {
 
 
     return (
+        <div id="main">
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>    
     )
 
 }
